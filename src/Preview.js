24f0c4efde658a2416d924f92961ca8e77f54d58
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-02-04 20:48:22 +0530

    Dev setup done with webpack,babel

---
+++ src/Preview.js
@@ -0,0 +1,23 @@
+// Libraries.
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Preview extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                dei
+            </div>
+        );
+    }
+
+}
+
+export default Preview;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Libraries.
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import Preview from "./Preview";
+
+ReactDOM.render(<Preview />, document.getElementById("index"));
