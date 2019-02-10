6000c759d5fbe25f7dd6059952735f86d5061066
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-02-10 13:33:24 +0530

    Adds test, next add readme

--- src/mock/InputComponent2.js
+++ src/mock/InputComponent2.js
@@ -1,11 +1,7 @@
 import style from "../Preview/Preview.css";
 import labelComponent from "./LabelComponent";
 
-<<<<<<< HEAD
 let markup = `<input type='text' id="secondInput" className={style.box} value={state.name} {...events}/>`;
-=======
-let markup = `<input type='text' className={style.box} value={state.name} {...events}/>`;
->>>>>>> 797846a344e932663d7ff121b8cc0956f5db6182
 
 let state = {
     name: "Second Component"
